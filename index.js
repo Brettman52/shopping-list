2cf9de363dc01d79650ac21679db6aa254981a3b
Brettman52 <61206038+Brettman52@users.noreply.github.com>
2020-04-06 13:20:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function handleSubmit() {
    
    
   
-   $(".shopping-list").append('<li><span class="shopping-item">' + addItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>')
+   $(".shopping-list").append('<li><span class="shopping-item">' + addItem + '</span><div class="shopping-item-controls"> + 
+    '<button class="shopping-item-toggle"><span class="button-label">check</span></button> ' +
+    ' <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>')
    
    $("input[type=text]").val('');
      
